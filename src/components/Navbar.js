f472485a1ad01fdc643634a2e899dc64335b8ce3
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-05 10:59:49 +0900

    Navbar component made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,46 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="nav">
-        <ul>
-          <li>Hi</li>
-          <li>Hi</li>
-          <li>Hi</li>
-          <li>Hi</li>
-        </ul>
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="" className="navbar-brand" />
+        </Link>
+        <button
+          className="navbar-toggler mr-auto"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Link
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="/cart">
+          <button>
+            <i className="fas fa-cart-plus">Cart</i>
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
