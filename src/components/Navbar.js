d8a6e55e5784ed2db7addd75256ae595d1a5daf2
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-05 10:26:25 +0900

    Routes added

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <ul>
+          <li>Hi</li>
+          <li>Hi</li>
+          <li>Hi</li>
+          <li>Hi</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <Product />
+      </div>
+    );
+  }
+}
+
+export default Navbar;
