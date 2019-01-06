a9cd25746d41aa9e4d918e4821b3554fc919ede4
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-06 17:51:57 +0900

    Context state modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 class Product extends Component {
   render() {
@@ -22,7 +23,7 @@ class Product extends Component {
               onClick={() => console.log("button clicked")}
             >
               {inCart ? (
-                <p className="text-capitalize mb-0" disapled>
+                <p className="text-capitalize mb-0" disabled>
                   In Cart
                 </p>
               ) : (
@@ -41,6 +42,16 @@ class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
   .card {
