92aaf0999181e390cc94de97d5ca7b30ac755087
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-06 22:24:15 +0900

    Handle Details functionality added

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,27 +10,35 @@ class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("clicked")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product image" className="card-img-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onClick={() => console.log("button clicked")}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  In Cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus" />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {value => (
+              <div
+                className="img-container p-5"
+                onClick={() => {
+                  value.handleDetail(id);
+                }}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product image" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => {
+                    value.addToCart(id);
+                  }}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      In Cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
             <h5 className="text-blue font-italic mb-0">
