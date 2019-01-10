91dcc68f422b8e48dfeb4a928981f1389448a179
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-10 14:37:35 +0900

    Add to cart functionality added and Modal component added

--- src/components/Details.js
+++ src/components/Details.js
@@ -48,7 +48,10 @@ export default class Details extends Component {
                   <ButtonContainer
                     cart
                     disabled={inCart ? true : false}
-                    onClick={() => value.addToCart(id)}
+                    onClick={() => {
+                      value.addToCart(id);
+                      value.openModal(id);
+                    }}
                   >
                     {inCart ? "inCart" : "Add to cart"}
                   </ButtonContainer>
